5efe4629ae5b402b07bb997079f20a1e048596df
Ohleesang <jammmmed@gmail.com>
2023-11-23 09:27:29 +0900

    런타임 에러 오류(더 효율적으로 짜야하나보다..)

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -2,6 +2,7 @@
 
 class Solution {
     fun solution(s: String): String {
+<<<<<<< HEAD
         var answer = ""
         var strList = s.split(" ").toMutableList()
         for(i in strList.indices){
@@ -9,11 +10,27 @@ class Solution {
             answer = answer + strList[i]+" "
         }
         answer = answer.trimEnd()//맨 뒷부분 공백 제거
+=======
+        //공백이 연속으로 나올때를 고려하지 않음;
+        var answer = ""
+        var result = StringBuilder()
+        var strList = s.split(" ").toMutableList()
+        while(strList.remove("")){}
+        for(i in strList.indices){
+            strList[i] = strList[i].first().uppercase() + strList[i].substring(1..strList[i].lastIndex).lowercase()
+            result = result.append("${strList[i]} ")
+        }
+        answer = result.toString().trimEnd()//맨 뒷부분 공백 제거
+>>>>>>> fdedbe9 (런타임 에러 오류(더 효율적으로 짜야하나보다..))
         return answer
     }
 }
 fun main(){
     var a = Solution()
+<<<<<<< HEAD
     a.solution("3people unFollowed me")
+=======
+    a.solution("3people  unFollowed  me")
+>>>>>>> fdedbe9 (런타임 에러 오류(더 효율적으로 짜야하나보다..))
     a.solution("for the last week")
 }
